f928d56bfb7e88a85f0ece41718a6026c8a7c347
glc3344 <glc3344@gmail.com>
2017-11-06 14:40:17 -0500

    Rearranged project structure and implemented browsersync and cssinjecting/reloading

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,33 @@ const gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
-  cssImport = require('postcss-import');
+  cssImport = require('postcss-import'),
+  browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   return gulp
-    .src('./assets/styles/styles.css')
+    .src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
-  watch('./assets/styles/**/*.css', function() {
-    gulp.start('styles');
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
   });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssReload');
+  });
+});
+
+gulp.task('cssReload', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
 });
